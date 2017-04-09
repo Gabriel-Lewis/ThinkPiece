f696aa2a7cdb7e72126cb51d328227c5127bd959
Gabriel Lewis <gabrielhlewis@gmail.com>
2017-04-08 19:31:44 -0700

    Removes Babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
         loader: 'babel',
         query: {
           presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['transform-decorators-legacy', 'transform-class-properties'],
         },
       },
     ],
